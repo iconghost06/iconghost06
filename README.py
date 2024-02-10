576227e608ef04950e9acd4900aeb46975d1b18b
iconghost06 <153359728+iconghost06@users.noreply.github.com>
2024-02-11 00:13:31 +0530

    Update README.py

--- README.py
+++ README.py
@@ -1,13 +1,3 @@
-import matplotlib.pyplot as plt 
-import numpy as np
-fig = plt.figure()
-ax = plt.axes()
-x = np.linspace(0, 10, 1000)
-ax.plot(x, np.sin(x));
-plt.plot(x, np.sin(x - 0), color='blue') # specify color by name
-plt.plot(x, np.sin(x - 1), color='g') # short color code (rgbcmyk)
-plt.plot(x, np.sin(x - 2), color='0.75') # Grayscale between 0 and 1
-plt.plot(x, np.sin(x - 3), color='#FFDD44') # Hex code (RRGGBB from 00 to FF) plt.plot(x, np.sin(x - 4), color=(1.0,0.2,0.3)) # RGB tuple, values 0 and 1 plt.plot(x, np.sin(x - 5), color='chartreuse');# all HTML color names supported
 
 
 <!---
